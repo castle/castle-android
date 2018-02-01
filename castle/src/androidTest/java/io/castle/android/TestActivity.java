dbe071220c90f845e311911687cca7212e23e9af
Sebastian Simson <sebastian@simsons.se>
2018-02-01 11:31:36 +0100

    Remove unused annotation

--- castle/src/androidTest/java/io/castle/android/TestActivity.java
+++ castle/src/androidTest/java/io/castle/android/TestActivity.java
@@ -2,14 +2,13 @@ package io.castle.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
 /**
  * Copyright (c) 2017 Castle
  */
 public class TestActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setTitle("TestActivity");
