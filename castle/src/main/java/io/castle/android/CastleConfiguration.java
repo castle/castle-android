04efe3556f54cd36ca6b2a6ff7de055829f7508f
Sebastian Simson <sebastian@simsons.se>
2019-01-31 13:03:56 +0100

    Simplified configuration defaults

--- castle/src/main/java/io/castle/android/CastleConfiguration.java
+++ castle/src/main/java/io/castle/android/CastleConfiguration.java
@@ -11,22 +11,24 @@ import java.util.List;
  * Copyright (c) 2017 Castle
  */
 public class CastleConfiguration {
-    private boolean debugLoggingEnabled = false;
+    private static final boolean DEFAULT_DEBUG_LOGGING_ENABLED = false;
+    private static final int DEFAULT_FLUSH_LIMIT = 20;
+    private static final int DEFAULT_MAX_QUEUE_LIMIT = 1000;
+    private static final boolean DEFAULT_SCREEN_TRACKING_ENABLED = true;
 
-    private int flushLimit = 20;
-    private int maxQueueLimit = 1000;
-
-    private String publishableKey;
-    private boolean screenTrackingEnabled = true;
-
+    private boolean debugLoggingEnabled;
+    private int flushLimit;
+    private int maxQueueLimit;
+    private boolean screenTrackingEnabled;
     private List<String> baseURLWhiteList;
+    private String publishableKey;
 
     public CastleConfiguration() {
         this(new Builder());
     }
 
     private CastleConfiguration(Builder builder) {
-        this.debugLoggingEnabled = builder.debugLoggingEnabled;
+        this.debugLoggingEnabled = builder.debugLoggingEnabled();
         this.flushLimit = builder.flushLimit();
         this.maxQueueLimit = builder.maxQueueLimit();
         this.publishableKey = builder.publishableKey();
@@ -67,10 +69,10 @@ public class CastleConfiguration {
         private List<String> baseURLWhiteList;
 
         public Builder() {
-            debugLoggingEnabled = false;
-            flushLimit = 20;
-            maxQueueLimit = 1000;
-            screenTrackingEnabled = true;
+            debugLoggingEnabled = DEFAULT_DEBUG_LOGGING_ENABLED;
+            flushLimit = DEFAULT_FLUSH_LIMIT;
+            maxQueueLimit = DEFAULT_MAX_QUEUE_LIMIT;
+            screenTrackingEnabled = DEFAULT_SCREEN_TRACKING_ENABLED;
         }
 
         public Builder(CastleConfiguration configuration) {
