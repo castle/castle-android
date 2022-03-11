ca6e17e8edf642179da42cf6eee1c9a9c777ccbd
Sebastian Simson <sebastian@simsons.se>
2022-03-11 17:18:05 +0100

    Remove support for maps in event properties

--- castle/src/main/java/io/castle/android/api/model/CustomEvent.java
+++ castle/src/main/java/io/castle/android/api/model/CustomEvent.java
@@ -54,21 +54,6 @@ public class CustomEvent extends Event {
         for (Map.Entry<String, Object> entry : properties.entrySet()) {
             Object value = entry.getValue();
 
-            // If the value is a Map call the method recursively
-            if (value instanceof Map) {
-                try {
-                    Map<String, Object> map = (Map<String, Object>) value;
-
-                    // If the contents aren't valid we skip the properties
-                    boolean valid = propertiesContainValidData(map);
-                    if (!valid) {
-                        return false;
-                    }
-                } catch (Exception e) {
-                    return false;
-                }
-            }
-
             // If the value of of any other type than NSNumber, NSString or NSNull: validation failed
             if (!(value instanceof String
                     || value instanceof Integer
