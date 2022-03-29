1c1494ac46845cf4a766983ed22a0b1eae0bf43f
Sebastian Simson <sebastian@simsons.se>
2022-03-29 09:14:30 +0200

    Add logic for device id source

--- castle/src/main/java/io/castle/android/DeviceIdUtils.java
+++ castle/src/main/java/io/castle/android/DeviceIdUtils.java
@@ -6,24 +6,28 @@ package io.castle.android;
 
 import android.media.MediaDrm;
 import android.os.Build;
+import android.util.Base64;
+import android.util.Pair;
 
 import java.security.MessageDigest;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
+import io.castle.highwind.android.Highwind;
+
 public class DeviceIdUtils {
     private final static Pattern UUID_REGEX_PATTERN = Pattern.compile("^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$");
     private static final UUID WIDEVINE_UUID = new UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L);
 
-    public static String deviceId() {
+    public static Pair<String, Integer> deviceId() {
         try {
             String wideVineId = wideVineId();
             if (isValidUUID(wideVineId)) {
-                return wideVineId;
+                return new Pair<>(wideVineId, Highwind.ID_SOURCE_WIDEVINE);
             }
         } catch (Exception ignored) {}
 
-        return UUID.randomUUID().toString();
+        return new Pair<>(UUID.randomUUID().toString(), Highwind.ID_SOURCE_GENERATED);
     }
 
     private static String wideVineId() {
