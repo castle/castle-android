4c31e719151a23bd7ee7b7c0621354fbc646705f
Sebastian Simson <sebastian@simsons.se>
2022-03-29 09:47:39 +0200

    Use md5 for hashing widevine and fix validation

--- castle/src/main/java/io/castle/android/DeviceIdUtils.java
+++ castle/src/main/java/io/castle/android/DeviceIdUtils.java
@@ -6,7 +6,6 @@ package io.castle.android;
 
 import android.media.MediaDrm;
 import android.os.Build;
-import android.util.Base64;
 import android.util.Pair;
 
 import java.security.MessageDigest;
@@ -16,13 +15,13 @@ import java.util.regex.Pattern;
 import io.castle.highwind.android.Highwind;
 
 public class DeviceIdUtils {
-    private final static Pattern UUID_REGEX_PATTERN = Pattern.compile("^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$");
+    private final static Pattern HEX_REGEX_PATTERN = Pattern.compile("^[0-9a-f]+$");
     private static final UUID WIDEVINE_UUID = new UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L);
 
     public static Pair<String, Integer> deviceId() {
         try {
             String wideVineId = wideVineId();
-            if (isValidUUID(wideVineId)) {
+            if (isValid(wideVineId)) {
                 return new Pair<>(wideVineId, Highwind.ID_SOURCE_WIDEVINE);
             }
         } catch (Exception ignored) {}
@@ -30,12 +29,12 @@ public class DeviceIdUtils {
         return new Pair<>(UUID.randomUUID().toString(), Highwind.ID_SOURCE_GENERATED);
     }
 
-    private static String wideVineId() {
+    static String wideVineId() {
         MediaDrm wvDrm = null;
         try {
             wvDrm = new MediaDrm(WIDEVINE_UUID);
             byte[] wideVineId = wvDrm.getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID);
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(wideVineId);
             return byteArrayToHexString(md.digest());
         } catch (Exception e) {
@@ -61,10 +60,14 @@ public class DeviceIdUtils {
         return buffer.toString();
     }
 
-    private static boolean isValidUUID(String uuid) {
-        if (uuid == null) {
+    private static boolean isValid(String hash) {
+        if (hash == null) {
             return false;
         }
-        return UUID_REGEX_PATTERN.matcher(uuid).matches();
+        if (hash.length() != 32) {
+            return false;
+        }
+        boolean matches = HEX_REGEX_PATTERN.matcher(hash).matches();
+        return matches;
     }
 }
